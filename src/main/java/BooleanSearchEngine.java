ed4575b6f2d5965063b286bd0a50e2025781568d
Korotkov Dmitry <2110821@mail.ru>
2023-02-09 19:50:14 +0300

    Implement searchEngine

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,52 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    Map<String, List<PageEntry>> wordsStatistics = new TreeMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
+        for (File pdfFile : pdfsDir.listFiles()) {
+            String pdfName = pdfFile.getName();
+            var doc = new PdfDocument(new PdfReader(pdfFile));
+            for (int i = 1; i < doc.getNumberOfPages(); i++) {
+                int pageNumber = i;
+                var page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> freqsInOnePage = new HashMap<>();
+                for (String word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqsInOnePage.put(word, freqsInOnePage.getOrDefault(word, 0) + 1);
+                }
+                freqsInOnePage.forEach((word, freq) ->
+                        {
+                            if (wordsStatistics.get(word) == null) {
+                                List<PageEntry> initialList = new ArrayList<>();
+                                initialList.add(new PageEntry(pdfName, pageNumber, freq));
+                                wordsStatistics.put(word, initialList);
+                            } else {
+                                List<PageEntry> listWithContent = wordsStatistics.get(word);
+                                listWithContent.add(new PageEntry(pdfName, pageNumber, freq));
+                                wordsStatistics.put(word, listWithContent);
+                            }
+                        }
+                );
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        return wordsStatistics.get(word.toLowerCase());
     }
 }
