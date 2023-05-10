a11493303ae04098c517246d4490dac61cb8c2d5
Ant722 <Ant722@mail.ru>
2023-05-10 09:32:12 +0300

    Дописал Main по первой части

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,45 +7,39 @@ import java.net.Socket;
 import java.util.Arrays;
 
 
-
 public class Main {
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File(ServerConfigurations.pdfsDir));
-        System.out.println(engine.search("стена"));
+//        System.out.println(engine.search("стена"));
 
-//        try (ServerSocket serverSocket = new ServerSocket(ServerConfigurations.PORT)) { // стартуем сервер один(!) раз
-//
-//            System.out.println("Сервер запущен");
-//
-//            while (true) { // в цикле(!) принимаем подключения
-//                try (
-//                        Socket socket = serverSocket.accept();
-//                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                        PrintWriter out = new PrintWriter(socket.getOutputStream());
-//                ) {
-//                    System.out.println("Новая покупка");
-//                    String str = in.readLine();
-//                    Gson gson = new Gson();
-//
-//
-//                    Purchase purchase = gson.fromJson(str, Purchase.class);
-//                    System.out.println(purchase.toString());
-//                    statistics.addPurchase(purchase);
-//
-//                    GsonBuilder builder = new GsonBuilder();
-//                    out.write(builder.setPrettyPrinting().create().toJson(statistics.statistic()));
-//                    System.out.println(builder.setPrettyPrinting().create().toJson(statistics.statistic()));
-//
-//
-//                } catch (IOException ex) {
-//                    throw new RuntimeException(ex);
-//                }
-//            }
-//        } catch (
-//                IOException | ClassNotFoundException e) {
-//            System.out.println("Не могу стартовать сервер");
-//            e.printStackTrace();
-//        }
+        try (ServerSocket serverSocket = new ServerSocket(ServerConfigurations.PORT)) { // стартуем сервер один(!) раз
+
+            System.out.println("Сервер запущен");
+
+            while (true) { // в цикле(!) принимаем подключения
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    System.out.println("Новый запрос");
+                    String str = in.readLine();
+                    System.out.println(str);
+
+
+                    GsonBuilder builder = new GsonBuilder();
+                    out.write(builder.setPrettyPrinting().create().toJson(engine.search(str)));
+                    System.out.println(builder.setPrettyPrinting().create().toJson(engine.search(str)));
+
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
 
     }
 }
