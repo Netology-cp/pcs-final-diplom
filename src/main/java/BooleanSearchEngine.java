0b87185ecbb13aae71f35e8d3dcd7c7e62e81a63
KrozhDev <roko.mdn@gmail.com>
2023-05-14 10:32:58 +0300

    first commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,3 +1,7 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -6,6 +10,19 @@ public class BooleanSearchEngine implements SearchEngine {
     //???
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
+//        File folder = new File("/path/to/folder");
+        File[] pdfFiles = pdfsDir.listFiles();
+        for (File pdfFile : pdfFiles) {
+            if (pdfFile.isFile()) {
+                System.out.println(pdfFile.getName());
+                var pdfDocument = new PdfDocument(new PdfReader(pdfFile));
+                System.out.println(pdfDocument.getNumberOfPages());
+                for (int page = 1; page <= pdfDocument.getNumberOfPages(); page++) {
+                    var text = PdfTextExtractor.getTextFromPage(pdfDocument.getPage(page));
+                    var words = text.split("\\P{IsAlphabetic}+");
+                }
+            }
+        }
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
     }
